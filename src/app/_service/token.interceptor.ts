680c55b927f01a3faa107d3b14b58b6ad5107f28
ilkinsufi <ilkinsufi@gmail.com>
2024-04-20 23:33:13 +0400

    NGRX (store & effects)

--- src/app/_service/token.interceptor.ts
+++ src/app/_service/token.interceptor.ts
@@ -1,7 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  // bu tokenin heç bir funksiyası yoxdur :)
   let _token = 'MRfxpLx8j1z4FT3tT93EeAYP5R0dezJP1zTcHj09hWzBVqaamfPo9zuQ34IRbbhs';
+  
 
   let jwtToken = req.clone({
     setHeaders: {
--- src/app/component/customer/customer.component.ts
+++ src/app/component/customer/customer.component.ts
@@ -2,17 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { MasterService } from '../../_service/master.service';
 import { Posts } from '../../../_model/posts';
 import { Customer } from '../../../_model/Customer';
+import { MaterialModule } from '../../../_module/Material.Module';
+import { MatTableDataSource } from '@angular/material/table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, CommonModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css',
 })
 export class CustomerComponent implements OnInit {
   constructor(private service: MasterService) {}
+
   customerdata!: Customer[];
+  datasource: any;
+  displayColums: string[] = ['code', 'name', 'email', 'phone'];
   ngOnInit(): void {
     this.LoadInitialData();
   }
@@ -20,7 +26,7 @@ export class CustomerComponent implements OnInit {
   LoadInitialData() {
     this.service.GetAllCustomer().subscribe((item) => {
       this.customerdata = item;
-      console.log(this.customerdata);
+      this.datasource = new MatTableDataSource(this.customerdata);
     });
   }
 }
